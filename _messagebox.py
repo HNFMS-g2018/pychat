beab9f4bfb74c44bf290f42fa6228147b83fd7c1
Kewth <wthdsg@foxmail.com>
2019-01-14 19:08:39 +0800

    add warning and error messagebox

--- _messagebox.py
+++ _messagebox.py
@@ -2,5 +2,16 @@
 import os
 
 def info(mes):
-    'print info on a new window'
-    os.system('zenity 2> /dev/null --info --title=\'pychat\' --text=\'' + mes + '\'')
+    'print [mes] on a new window'
+    os.system('zenity 2> /dev/null --info \
+            --title=\'pychat\' --text=\'' + mes + '\'')
+
+def warning(mes):
+    'print [mes] on a new windows as a warning'
+    os.system('zenity 2> /dev/null --warning \
+            --title=\'warning from pychat\' --text=\'' + mes + '\'')
+
+def error(mes):
+    'print [mes] on a new windows as an error'
+    os.system('zenity 2> /dev/null --error \
+            --title=\'warning from pychat\' --text=\'' + mes + '\'')
